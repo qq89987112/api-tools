625d80c327278cc6e3cbb9ec859b45989c7de1af
蔡荣庚 <89987112@qq.com>
2017-11-24 18:17:31 +0800

    完成了itemTemplate

---
+++ src/code-gen/index.js
@@ -0,0 +1,25 @@
+let
+    _ = require("lodash"),
+    jsBeautify = require('js-beautify').js,
+    fse = require('fs-extra'),
+    path = require('path'),
+    itemTemplate = fse.readFileSync(path.join(__dirname,'./template/item')),
+    mainTemplate = fse.readFileSync(path.join(__dirname,'./template/main')),
+    apis = require('../config.json');
+
+
+
+    apis.forEach((item)=>{
+        let itemResult = _.template(itemTemplate)({apis:item.value,getParams:(item)=>{
+            if(item.method.toUpperCase() === 'GET' && item.params){
+                let results = item.params.map((item)=>{
+                    return `${item}=\$\{${item}\}`;
+                });
+                return `?${results.join('&')}`
+            }
+            return '';
+        }});
+
+        console.log(itemResult);
+    });
+
