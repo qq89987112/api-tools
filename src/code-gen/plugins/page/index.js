bec53a8d5a5709a61f4aaacf6316291ce46a3bfc
蔡荣庚 <89987112@qq.com>
2017-11-25 17:01:50 +0800

    add import relative,wait api page separate

--- src/code-gen/plugins/page/index.js
+++ src/code-gen/plugins/page/index.js
@@ -40,6 +40,7 @@ module.exports = (app) => {
                     pageName = item[0],
                     apis = item[1],
                     pageOutput = `./dist/page/${pageName}.vue`,
+
                     //  拿到所有 api 的依赖地址
                     dependencies = new Set(apis.map((item)=>{
                         return  `import ${item.fileName} from '${path.relative(path.dirname(pageOutput),path.dirname(apiFiles[item.fileName])).replace(/\\/g,"/")+`/${item.fileName}.js`}'`
