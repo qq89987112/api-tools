7ae6d0a7fb036f2bc366e05645fae697fd132a7f
蔡荣庚 <89987112@qq.com>
2017-11-25 20:04:30 +0800

    update project structure

--- src/plugins/test-api/index.js
+++ src/plugins/test-api/index.js
@@ -6,7 +6,11 @@ let
         switch (item.method.toUpperCase()){
             case "GET":
                 temp = request
-                    .get(item.url);
+                    .get(item.url,{
+                        qs:{
+
+                        }
+                    });
                 break;
             case "POST":
                 break;
@@ -21,22 +25,39 @@ let
                 done();
                 console.log(err);
             });
-    },5);
+    },5),
+    states = {},
+    counter = new Set();
 
 module.exports = (app)=>{
+    let
+        apis = app.$data,
+        counter = new Set();
 
-    return ()=>{
-        let apis = app.$data
-        apis.forEach((item)=>{
-            let
-                apiConfigs = item.value;
-
-                apiConfigs.forEach((item)=>{
-                    requests.push(item);
-                })
-
+    apis.forEach((item)=>{
+        let apiConfigs = item.value||[];
+        apiConfigs.forEach((item)=>{
+            if(item.url){
+                counter.push(item.url);
+            }
         });
+    });
 
-    }
+    app.$on("api-result",(results)=>{
 
+        apis.forEach((item)=>{
+            let state = states[item.url];
+            if(state!=="success"&&state!=="doing"){
+                let
+                    apiConfigs = item.value;
+
+                    apiConfigs.forEach((item)=>{
+                        //先进行参数检查,无外部依赖或者参数获取不报错的进入请求。
+
+                        //
+                        requests.push(item);
+                    })
+            }
+        });
+    });
 };
