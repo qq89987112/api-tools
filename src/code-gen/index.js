17c88d78bc44f1d8f2ca2d1743b5a47f3b6d6b4b
蔡荣庚 <89987112@qq.com>
2017-11-25 14:53:19 +0800

    add import relative

--- src/code-gen/index.js
+++ src/code-gen/index.js
@@ -13,10 +13,13 @@ let
 
 module.exports = (apis) => {
     fse.removeSync("./dist");
-    apis.forEach((item) => {
-        let fileApis =item.value;
-        if(fileApis){
+    let apiFiles = apis.reduce((prev,item)=>Object.assign(prev,{[item.name]:`./dist/api/${item.name}.js`}),{});
 
+    apis.forEach((item) => {
+        let
+            fileApis =item.value;
+
+        if(fileApis){
             let itemResult = _.template(itemTemplate)({
                 apis: fileApis,
                 getParams: (item) => {
@@ -30,40 +33,45 @@ module.exports = (apis) => {
                 }
             });
 
-            fse.outputFileSync(`./dist/api/${item.name}.js`, jsBeautify(itemResult,beautifyOps));
+            fse.outputFileSync(apiFiles[item.name], jsBeautify(itemResult,beautifyOps));
 
+
+            // 生成page vue页面
             let
                 pages = {};
-
+                // 把所有指向同一个page的api抽取出来
                 fileApis.reduce((pages,api)=>{
                     if(api.page){
-
                         api = JSON.parse(JSON.stringify(api));
                         api.fileName = item.name;
                         let page = pages[api.page] || [];
                         page.push(api);
                         pages[api.page] = page;
                     }
-
                     return pages;
                 },pages);
-
                 Object.entries(pages).forEach((item)=>{
+
                     let
                         pageName = item[0],
                         apis = item[1],
+                        pageOutput = `./dist/page/${pageName}.vue`,
+                        dependencies = new Set(apis.map((item)=>{
+                           return  `import ${item.fileName} from '${path.relative(path.dirname(pageOutput),path.dirname(apiFiles[item.fileName])).replace(/\\/g,"/")+`/${item.fileName}.js`}'`
+                        })),
                         content = _.template(vueTemplate)({
-                            page:{
-                                name:pageName,
-                                apis
-                            }
+                            name:pageName,
+                            dependencies:Array.from(dependencies),
+                            apis
                         });
-                        fse.outputFileSync(`./dist/page/${pageName}.vue`,content.replace(/<script>([\s\S]+)<\/script>/,(matched,extract,index,all)=>{
+                        //  拿到所有 api 的依赖地址
+
+
+                        // 开始生成
+                        fse.outputFileSync(pageOutput,content.replace(/<script>([\s\S]+)<\/script>/,(matched,extract,index,all)=>{
                             return matched.replace(extract, `\r\n${jsBeautify(extract,beautifyOps)}\r\n`);
                         }));
                 });
-
-
         }
 
 
