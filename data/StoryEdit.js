cf67f9bdf19be41558da3e36c43e9521fc834c93
蔡荣庚 <89987112@qq.com>
2018-01-29 23:23:34 +0800

    update

--- data/StoryEdit.js
+++ data/StoryEdit.js
@@ -9,15 +9,20 @@ import AvatarUploader from "./ant-custom/AvatarUploader";
 
 export default class StoryEdit extends BaseComponent {
     render(){
+        // title
+        // subTitle
+        // photoUrl
+        // resUrl
+        // updateTime
         const onSubmit = this.props.onSubmit;
         return  <Form onSubmit={e=>{
             e.preventDefault();
             this.$load('submit');
-            onSubmit&&onSubmit().then(()=>this.$cancel('submit'));
+            onSubmit&&onSubmit(this.$getInputValue(['subTitle','updateTime'])).then(()=>this.$cancel('submit'));
         }}>
             <Form.Item {...FormUtils.formItemLayout(1)} label='设置封面'><AvatarUploader/></Form.Item>
-            <Form.Item {...FormUtils.formItemLayout(1)} label='副标题'><Input/></Form.Item>
-            <Form.Item {...FormUtils.formItemLayout(1)} label='更新日期'><DatePicker/></Form.Item>
+            <Form.Item {...FormUtils.formItemLayout(1)} label='副标题'><Input onInput={this.$onInput('subTitle')} /></Form.Item>
+            <Form.Item {...FormUtils.formItemLayout(1)} label='更新日期'><DatePicker onChange={d=>this.$setInputValue('updateTime',d)} /></Form.Item>
             <Form.Item className='tac'><Button htmlType='submit' type='primary'>保存</Button></Form.Item>
         </Form>
     }
