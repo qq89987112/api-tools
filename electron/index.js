de462dcf89f1a5f4f27984f21fc06bdefb08d0b7
蔡荣庚 <89987112@qq.com>
2018-02-02 14:35:20 +0800

    update

--- electron/index.js
+++ electron/index.js
@@ -21,8 +21,7 @@ const createWindow = () => {
             // in an array if your app supports multi windows, this is the time
             // when you should delete the corresponding element.
             mainWindow = null;
-        })
-        ;
+        });
     }
 ;
 
