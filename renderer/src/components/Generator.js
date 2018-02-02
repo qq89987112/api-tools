6add41e02f573c267338e2f30b0b67f710d099fb
蔡荣庚 <89987112@qq.com>
2018-02-02 18:13:49 +0800

    update

--- renderer/src/components/Generator.js
+++ renderer/src/components/Generator.js
@@ -1,26 +1,34 @@
 import React, { Component } from 'react';
-import {Form} from 'antd'
+import {Form,Input} from 'antd'
 import 'antd/dist/antd.css';
 
 
 export default class Generator extends Component {
 
+    onInput(key){
+
+    }
+
     render() {
         const {parameter = {}} = this.props;
-
+        const fields = Object.entries(parameter).filter(item=>!~item.indexOf('-type')).reduce((prev,cur)=>{
+            prev[cur[0]] = parameter[`${cur[0]}-type`];
+            return prev;
+        },{})
         return (
             <Form layout='inline' onSubmit={e=>{
                 e.preventDefault();
             }}>
                 {
-                    Object.entries(parameter).map(item=>
-                        <Form.Item label={item[0]}>
+                    Object.entries(fields).map(item=>{
+                        const type = (item[1]||"").toLowerCase();
+                        return <Form.Item label={item[0]}>
                             {
-                                item[1]
+                                type ==='string'&&<Input/>
                             }
-                        </Form.Item>)
+                        </Form.Item>
+                    })
                 }
-
             </Form>
         );
     }
