dad3677ca5251ca85268854a2f44cd341fbc1635
蔡荣庚 <89987112@qq.com>
2018-02-03 11:47:14 +0800

    update

--- renderer/src/components/ApiManager/JSONResult.js
+++ renderer/src/components/ApiManager/JSONResult.js
@@ -7,16 +7,45 @@ const TreeNode = Tree.TreeNode;
 
 export default class JSONResult extends Component {
 
-    loop(obj, path = "") {
+    // loop(obj, path = "") {
+    //
+    //     function isValid(obj) {
+    //         let type = Object.prototype.toString.call(obj);
+    //         return type === "[object Object]" || type === "[object Array]";
+    //     }
+    //
+    //
+    //
+    //     if (isValid(obj)) {
+    //         return Object.entries(obj).map(item => {
+    //                 if (isValid(item[1])) {
+    //                     return <TreeNode title={item[0]} key={`${path}.${item[0]}`}>
+    //                         {this.loop(item[1], `${path}.${item[0]}`)}
+    //                     </TreeNode>
+    //                 } else {
+    //                     return <TreeNode title={`${path}：${obj}`} key={`${path}`}/>
+    //                 }
+    //             }
+    //         )
+    //     } else {
+    //         return <TreeNode title={`${path}：${obj}`} key={`${path}`}/>
+    //     }
+    //
+    // }
+
+
+    loop(title,key, path = "") {
 
         function isValid(obj) {
             let type = Object.prototype.toString.call(obj);
             return type === "[object Object]" || type === "[object Array]";
         }
 
-        if (isValid(obj)) {
+        return <TreeNode title={title} key={key}></TreeNode>
+
+        if (isValid(value)) {
             return Object.entries(obj).map(item => {
-                    if (isValid(item)) {
+                    if (isValid(item[1])) {
                         return <TreeNode title={item[0]} key={`${path}.${item[0]}`}>
                             {this.loop(item[1], `${path}.${item[0]}`)}
                         </TreeNode>
@@ -28,7 +57,6 @@ export default class JSONResult extends Component {
         } else {
             return <TreeNode title={`${path}：${obj}`} key={`${path}`}/>
         }
-
     }
 
     render() {
