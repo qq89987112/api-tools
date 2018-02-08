eb9f0f8bc042f9b85918321778616277e4160e72
蔡荣庚 <89987112@qq.com>
2018-02-08 09:44:15 +0800

    update

--- template/vue/distribution-record.vue
+++ template/vue/distribution-record.vue
@@ -1,53 +1,54 @@
 <template>
-  <loading class="consum-record-page" ref="loading">
-    <div class="list" v-load-more="loadNext">
-        <div class="list-item">
+    <loading class="consum-record-page" ref="loading">
+        <div class="list" v-load-more="loadNext">
+            <div class="list-item">
 
+            </div>
         </div>
-    </div>
-  </loading>
+    </loading>
 </template>
 
 <script>
 
-  import service from "../../js/api/service";
+    import service from "../../js/api/service";
 
-  export default {
-    components: {
-    },
-    name: 'consum-record',
-    data() {
-      return {
-        list: []
-      }
-    },
-    created() {
+    export default {
+        components: {
+        },
+        name: 'consum-record',
+        data() {
+            return {
+                list: []
+            }
+        },
+        created() {
 
-    },
-    mounted() {
-    },
-    methods: {
-      reLoad(){
-        this.params = {
-          page:1,
-          rows:10
-        };
-      },
-      loadNext(){
-        ++this.params.page;
-        this.loadPage(this.params);
-      },
-      loadPage(page){
-        this.params.page = page;
-        service.dispatchList(this.params)
-      }
+        },
+        mounted() {
+        },
+        methods: {
+            reLoad(){
+                this.params = {
+                    page:1,
+                    rows:10
+                };
+                this.loadPage(this.params);
+            },
+            loadNext(){
+                ++this.params.page;
+                this.loadPage(this.params);
+            },
+            loadPage(page){
+                this.params.page = page;
+                service.dispatchList(this.params)
+            }
+        }
     }
-  }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" rel="stylesheet/scss">
-  .consum-record-page {
-    height: 100vh !important;
-  }
+    .consum-record-page {
+        height: 100vh !important;
+    }
 </style>
