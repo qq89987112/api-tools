052243def9f692f679639e0183eae0bacda8ae6f
蔡荣庚 <89987112@qq.com>
2018-02-08 09:43:34 +0800

    update

---
+++ template/vue/distribution-record.vue
@@ -0,0 +1,53 @@
+<template>
+  <loading class="consum-record-page" ref="loading">
+    <div class="list" v-load-more="loadNext">
+        <div class="list-item">
+
+        </div>
+    </div>
+  </loading>
+</template>
+
+<script>
+
+  import service from "../../js/api/service";
+
+  export default {
+    components: {
+    },
+    name: 'consum-record',
+    data() {
+      return {
+        list: []
+      }
+    },
+    created() {
+
+    },
+    mounted() {
+    },
+    methods: {
+      reLoad(){
+        this.params = {
+          page:1,
+          rows:10
+        };
+      },
+      loadNext(){
+        ++this.params.page;
+        this.loadPage(this.params);
+      },
+      loadPage(page){
+        this.params.page = page;
+        service.dispatchList(this.params)
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" rel="stylesheet/scss">
+  .consum-record-page {
+    height: 100vh !important;
+  }
+</style>
