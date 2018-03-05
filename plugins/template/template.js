85fc7e02bf0ba18c03379e8e7fe4102fed82ba2a
蔡荣庚 <89987112@qq.com>
2018-03-05 15:36:08 +0800

    update

--- plugins/template/template.js
+++ plugins/template/template.js
@@ -2,6 +2,18 @@ function template() {
 
     return {
         parameters:{
+            code: String,
+        },
+        requestLib:{
+
+        },
+        compile(params) {
+            const {code=''} = params;
+            return `
+function template() {
+
+    return {
+        parameters:{
             className: String,
             labels: Array,
             fields: Array,
@@ -13,15 +25,19 @@ function template() {
         compile(params) {
             const {fields =[], className="", fieldValids=[], labels=[]} = params;
             const
-                className2 = className.replace(/^\S/,s=>s.toLowerCase()),
+                className2 = className.replace(/^\\S/,s=>s.toLowerCase()),
                 fieldsTypeMap = fields.reduce((prev,cur)=>{
                     cur = cur.split(".");
                     prev[cur[0]] = cur.slice(-1)[0];
                     return prev;
                 },{});
 
-            return `
-                  
+            return \`
+${code}
+            \`
+        }
+    }
+}
             `
         }
     }
