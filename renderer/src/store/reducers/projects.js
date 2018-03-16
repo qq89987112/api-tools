8299d2d11a5c15bad8855d81ad808e8a673f3278
蔡荣庚 <89987112@qq.com>
2018-03-16 15:11:35 +0800

    update

--- renderer/src/store/reducers/projects.js
+++ renderer/src/store/reducers/projects.js
@@ -1,26 +1,39 @@
 import {Shortcut} from "./shortcuts";
+import store from "../index"
 
 /**
  * [
- *  {
- *      shortcuts:{
- *          path:{
- *              activeGroup:""
- *          }
- *      }
- *  }
+ *    {
+ *       projectPath:''
+ *       activePath:'',
+ *       expandedKeys:[]
+ *     }
  * ]
  * @param state
  * @param action
  * @returns {any}
  */
 export default function (state = JSON.parse(localStorage.projects||'[]'),action){
-    let template;
+    let project;
     switch(action.type){
         // 可以更新shortcut group
         case "PROJECT_UPDATE":
-            break;
         case "PROJECT_ADD":
+            project = action.project;
+            const index = state.findIndex(t=>t.projectPath===project.path);
+            if (~index) {
+                let existProject = state[index];
+                if (existProject.activePath !== project.activePath) {
+                    store.dispatch({
+                        type:'SHORTCUT_RESET',
+                        1
+                    });
+                }
+                state[index] = project;
+            }else{
+                state = state.concat(project);
+            }
+            state = [...state];
             break;
         default:
             ;
--- renderer/src/store/reducers/shortcuts.js
+++ renderer/src/store/reducers/shortcuts.js
@@ -69,6 +69,8 @@ export class Shortcut {
     }
 }
 
+
+
 /**
  *
  * @param state
