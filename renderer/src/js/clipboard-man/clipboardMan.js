44a9428934628904ae5251fc759eccd7aefbd587
蔡荣庚 <89987112@qq.com>
2018-05-10 18:06:25 +0800

    update

--- renderer/src/js/clipboard-man/clipboardMan.js
+++ renderer/src/js/clipboard-man/clipboardMan.js
@@ -89,6 +89,10 @@ const utils = {
 
 
 export default function () {
+    // 2018-05-10
+    // 新增行情接口
+    // http://hq.sinajs.cn/list=M0
+    //
 
     // 2018-4-28
     // $css`<div class="a">                                 .a{
@@ -223,146 +227,155 @@ export default function () {
 
 
     try {
+        // 优先判断的指令
+        // $template
+        // $css
+        // $stock.m0
+
         let
             lineReg = /.+/g,
+            headCommands = {
+                //$css
+                [/^\$css`([\s\S]+?)`/]:function (result) {
+
+                    let html = parse(result[1]);
+                    function generateCss(elements=[]){
+                        // 对象里头的函数代码无法被编译成低版本的JS？
+                        elements = elements || [];
+                        return elements.map(item=>{
+                            let attributes = item.attributes || [];
+                            let css = attributes.find(i=>i.key==='class');
+                            if(css){
+                                css = css.value;
+                            }
+                            return css?`.${css}{
+                            ${generateCss(item.children)}
+                        }`:generateCss(item.children)
+                        }).filter(i=>i).join("\r\n")
+                    }
+                    // 当生成成功时,去掉$css``
+                    let cssResult = generateCss(html);
+                    console.log(cssResult);
+                    output(result[1]);
+                    clipboard.writeText(cssResult);
+                },
+                //$template
+                [/\$template`([\s\S]+?)`\?(?:file=(\S+))?/]:function (result) {
+                    let
+                        templateOption = {
+                            template: result[1],
+                            notify: {}
+                        },
+                        file = result[2],
+                        noticeReg = /\$\$(.+?)`([\s\S]+?)`/g,
+                        paramsReg = /\$params\?.+/,
+                        testReg = /\$test\??.*/,    // ? 可有可无
+                        paramsResult = paramsReg.exec(clipboardContent),
+                        testResult = testReg.exec(clipboardContent),
+                        noticeResult,
+                        name, params;
+
+                    // function toObject(paramsResult) {
+                    //     return paramsResult[1].split("&").reduce((prev, cur) => {
+                    //         cur = cur.split("=");
+                    //         prev[cur[0]] = cur[1];
+                    //         return prev;
+                    //     }, {});
+                    // }
+                    if (testResult) {
+                        [name, params] = utils.parseLineToObject(testResult[0]);
+                        templateOption.test = params;
+                    }
+
+                    while (noticeResult = noticeReg.exec(clipboardContent)) {
+                        templateOption.notify[noticeResult[1]] = noticeResult[2];
+                    }
+
+
+                    if (paramsResult) {
+                        [name, params] = utils.parseLineToObject(paramsResult[0]);
+                        templateOption.params = params;
+                    }
+
+
+                    if (templateOption.test) {
+                        result = templateMaker.make({
+                            template: `${templateOption.template}
+                        以上是生成的测试内容,生成文件需要(非必须)的$params就是$test时的所用的内容。
+                    `,
+                            params: Object.entries(templateOption.test || {}).reduce((prev, cur) => {
+                                return prev.concat({
+                                    name: cur[0],
+                                    type: "Object"
+                                })
+                            }, []),
+                            notices: templateOption.notify,
+                            defaultValues: templateOption.test
+                        });
+                        output(eval(`(${result})()`).compile({}, context));
+                    } else {
+                        if (!file) {
+                            context.error("请指定file 或者 先测试。");
+                            return;
+                        }
+                        // if (!templateOption.params) {
+                        //     context.error("请指定$params 您可以将 $test 直接改为 $params");
+                        //     return;
+                        // }
+                        let fileAddr = path.join(remote.getGlobal("__dirname"), "../plugins/template/single-file", file);
+                        result = templateMaker.make({
+                            template: templateOption.template,
+                            params: Object.entries(templateOption.params || {}).reduce((prev, cur) => {
+                                let value = cur[1];
+                                value = Object.prototype.toString.call(value).slice(8, -1);
+                                return prev.concat({
+                                    name: cur[0],
+                                    type: value
+                                })
+                            }, []),
+                            notices: templateOption.notify,
+                        });
+                        let promise = Promise.resolve();
+                        if (fse.existsSync(fileAddr)) {
+                            promise = keyboard.options(`文件路径已经存在文件:${fileAddr},是否继续？`, ["是", "否"]).then((selects) => {
+                                let select = selects[0];
+                                if (select === 0) {
+                                    return Promise.resolve();
+                                } else {
+                                    return Promise.reject();
+                                }
+                            });
+                        }
+                        promise.then(() => {
+                            fse.outputFileSync(fileAddr, jsBeautify.js(result));
+                            // 将这个 生成成功 替换为把 template语法去掉之后的原代码
+                            keyboard.output("生成成功");
+                        })
+                    }
+                },
+                //$stock.M0
+                [/^\$stock\.(.+)/]:function (result) {
+
+                }
+            },
             noticeReg = /\$\$([0-19])/,
-            templateReg = /\$template`([\s\S]+?)`\?(?:file=(\S+))?/,
-            cssReg = /^\$css`([\s\S]+?)`/,//\$[^\$]\S*
-            classTag = /<[^\/\n]+[^:]class="(.+?)"[^\/\n]*>/,
             multiParamsReg = /`([\s\S]+?)`/g,
             line,
             variables = {},
             commandOption = {};
 
 
-        // 优先判断的指令
-        // $template
-        // $css
-
-        let templateResult = templateReg.exec(clipboardContent);
-
-        if (templateResult) {
-
-            let
-                templateOption = {
-                    template: templateResult[1],
-                    notify: {}
-                },
-                file = templateResult[2],
-                noticeReg = /\$\$(.+?)`([\s\S]+?)`/g,
-                paramsReg = /\$params\?.+/,
-                testReg = /\$test\??.*/,    // ? 可有可无
-                paramsResult = paramsReg.exec(clipboardContent),
-                testResult = testReg.exec(clipboardContent),
-                noticeResult,
-                name, params;
-
-            // function toObject(paramsResult) {
-            //     return paramsResult[1].split("&").reduce((prev, cur) => {
-            //         cur = cur.split("=");
-            //         prev[cur[0]] = cur[1];
-            //         return prev;
-            //     }, {});
-            // }
-            if (testResult) {
-                [name, params] = utils.parseLineToObject(testResult[0]);
-                templateOption.test = params;
+        if (Object.entries(headCommands).find(i => {
+            let reg = eval(i[0]);
+            let result = reg.exec(clipboardContent)
+            if (result) {
+                i[1](result);
             }
-
-            while (noticeResult = noticeReg.exec(clipboardContent)) {
-                templateOption.notify[noticeResult[1]] = noticeResult[2];
-            }
-
-
-            if (paramsResult) {
-                [name, params] = utils.parseLineToObject(paramsResult[0]);
-                templateOption.params = params;
-            }
-
-
-            if (templateOption.test) {
-                templateResult = templateMaker.make({
-                    template: `${templateOption.template}
-                        以上是生成的测试内容,生成文件需要(非必须)的$params就是$test时的所用的内容。
-                    `,
-                    params: Object.entries(templateOption.test || {}).reduce((prev, cur) => {
-                        return prev.concat({
-                            name: cur[0],
-                            type: "Object"
-                        })
-                    }, []),
-                    notices: templateOption.notify,
-                    defaultValues: templateOption.test
-                });
-                output(eval(`(${templateResult})()`).compile({}, context));
-            } else {
-                if (!file) {
-                    context.error("请指定file 或者 先测试。");
-                    return;
-                }
-                // if (!templateOption.params) {
-                //     context.error("请指定$params 您可以将 $test 直接改为 $params");
-                //     return;
-                // }
-                let fileAddr = path.join(remote.getGlobal("__dirname"), "../plugins/template/single-file", file);
-                templateResult = templateMaker.make({
-                    template: templateOption.template,
-                    params: Object.entries(templateOption.params || {}).reduce((prev, cur) => {
-                        let value = cur[1];
-                        value = Object.prototype.toString.call(value).slice(8, -1);
-                        return prev.concat({
-                            name: cur[0],
-                            type: value
-                        })
-                    }, []),
-                    notices: templateOption.notify,
-                });
-                let promise = Promise.resolve();
-                if (fse.existsSync(fileAddr)) {
-                    promise = keyboard.options(`文件路径已经存在文件:${fileAddr},是否继续？`, ["是", "否"]).then((selects) => {
-                        let select = selects[0];
-                        if (select === 0) {
-                            return Promise.resolve();
-                        } else {
-                            return Promise.reject();
-                        }
-                    });
-                }
-                promise.then(() => {
-                    fse.outputFileSync(fileAddr, jsBeautify.js(templateResult));
-                    // 将这个 生成成功 替换为把 template语法去掉之后的原代码
-                    keyboard.output("生成成功");
-                })
-            }
-
+            return result;
+        })) {
             return;
         }
-        else {
-            let
-                htmlResult = cssReg.exec(clipboardContent);
-            if (htmlResult) {
-                let html = parse(htmlResult[1]);
 
-                function generateCss(elements=[]){
-                    return elements.map(item=>{
-                        let attributes = item.attributes || [];
-                        let css = attributes.find(i=>i.key==='class');
-                        if(css){
-                            css = css.value;
-                        }
-                        return css?`.${css}{
-                            ${generateCss(item.children)}
-                        }`:generateCss(item.children)
-                    }).filter(i=>i).join("\r\n")
-                }
-                // 当生成成功时,去掉$css``
-                output(htmlResult[1]);
-                let cssResult = generateCss(html);
-                console.log(cssResult);
-                clipboard.writeText(cssResult);
-                return;
-            }
-        }
 
 
         // 多行参数命令特殊,需要放在代码前头处理。
