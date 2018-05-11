e62c53ba05f5726f8b129761fbd22952a16e9beb
蔡荣庚 <89987112@qq.com>
2018-05-11 16:19:25 +0800

    update

---
+++ plugins/template/single-file/axios/config.js
@@ -0,0 +1,25 @@
+function template() {
+
+    return {
+        parameters: {
+
+        },
+        requestLib: {
+
+        },
+        //放在文件夹里时有用
+        events: {},
+        compile(params, context) {
+            const {} = params;
+
+            context && context.notify(undefined, undefined, {
+
+            });
+            return `
+            axios.interceptors.response.use(res=>{
+  return res.data.data;
+})
+        `
+        }
+    }
+}
