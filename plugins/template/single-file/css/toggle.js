70d6e3ed9ed4222c504d73132014fb8ad9dc80d6
蔡荣庚 <89987112@qq.com>
2018-05-15 16:13:52 +0800

    update

---
+++ plugins/template/single-file/css/toggle.js
@@ -0,0 +1,38 @@
+function template() {
+
+    return {
+        parameters: {
+
+        },
+        requestLib: {
+
+        },
+        //放在文件夹里时有用
+        events: {},
+        compile(params, context) {
+            const {} = params;
+
+            context && context.notify(undefined, undefined, {
+
+            });
+            return `
+            .toggle{
+          position: absolute;
+          top: 50%;
+          right: 0;
+          transform: translate(50%,-50%);
+          z-index: 2;
+          border: 1px solid #20a0ff;
+          border-radius: 50%;
+          width: 25px;
+          line-height: 25px;
+          text-align: center;
+          background-color: #20a0ff;
+          color: white;
+          box-shadow: 0 0 10px #666;
+          cursor: pointer;
+        }
+        `
+        }
+    }
+}
