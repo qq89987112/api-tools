8a4f8e3686f4e3bfe0741dd843930cf894514618
蔡荣庚 <89987112@qq.com>
2018-05-22 17:05:11 +0800

    update

--- plugins/template/single-file/axios/config.js
+++ plugins/template/single-file/axios/config.js
@@ -16,11 +16,11 @@ function template() {
 
             });
             return `
- import axios from "axios"
+import axios from "axios"
 import { Notification } from 'element-ui';
 
 if(localStorage.debug){
-    axios.defaults.baseURL = 'http://192.168.1.112:8076';
+    axios.defaults.baseURL = 'http://192.168.1.135:8076/';
 }
 
 axios.interceptors.request.use(config => {
@@ -31,13 +31,14 @@ axios.interceptors.request.use(config => {
 axios.interceptors.response.use(res => {
   // 后面是匹配没有遵循code data 格式的
   let data = res.data.data || res.data;
+  let message = res.message || res.data.message;
 
   // 遵循 code data 的需要200才能对，不遵循的直接放过
   if (res.data.status || res.data.code===undefined) {
     return data;
   } else {
     Notification({
-      message: data.message,
+      message,
       type: 'error'
     })
     return Promise.reject(data);
@@ -46,8 +47,9 @@ axios.interceptors.response.use(res => {
   res = res.response;
   // 后面是匹配没有遵循code data 格式的
   let data = res.data.data || res.data;
+  let message = res.message || res.data.message;
   Notification({
-    message: data.message,
+    message,
     type: 'error'
   })
   return Promise.reject(res.data);
