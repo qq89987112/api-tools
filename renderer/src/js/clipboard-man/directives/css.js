a9241560eac1edf7fb106f75012594a23bd64f52
蔡荣庚 <89987112@qq.com>
2018-05-23 16:07:18 +0800

    update

--- renderer/src/js/clipboard-man/directives/css.js
+++ renderer/src/js/clipboard-man/directives/css.js
@@ -1,8 +1,18 @@
 import keyboard from "../js/keyboard";
 const {clipboard, remote} = window.require('electron');
 const glob = remote.require("glob").sync;
-const {parse} = remote.require("himalaya");
-
+const {parse,stringify} = remote.require("himalaya");
+/**
+ * test
+ * 
+ *  $css`
+ *      <div style="margin-right:10px;" class="a" >
+ *          <div class="b" style="background:red;"></div>
+ *      </div>
+ *  `
+ * 
+ * 
+ */
 export default {
     validate:/^\$css`([\s\S]+?)`/,
     handle(result){
@@ -15,18 +25,23 @@ export default {
             return elements.map(item => {
                 let attributes = item.attributes || [];
                 let css = attributes.find(i => i.key === 'class');
+                let style = attributes.find(i => i.key === 'style');
                 if (css) {
                     css = css.value;
                 }
-                return css ? `.${css}{
-                            ${generateCss(item.children)}
+                if (style) {
+                    style = style.value;
+                }
+                return css ? `.${css}{${style?`
+                            ${style}
+                            `:''}${generateCss(item.children)}
                         }` : generateCss(item.children)
             }).filter(i => i).join("\r\n")
         }
 
-        // 当生成成功时,去掉$css``
+        // 当生成成功时,去掉$css`` style=""
         let cssResult = generateCss(html);
         console.log(cssResult);
-        keyboard.output(result[1]);
+        keyboard.output(result[1].replace(/style=["'].+?["'] ?/g,""));
         clipboard.writeText(cssResult);
     }}
